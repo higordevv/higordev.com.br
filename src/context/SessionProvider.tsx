c3b2e6b515f4dac54093d226d19b35b8c322c978
higordevv <pedrohigor2004@gmail.com>
2025-06-03 00:00:32 -0300

    Restater

--- src/context/SessionProvider.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-import { SessionProvider as Auth } from "next-auth/react";
-
-export function SessionProvider({ children }: { children: React.ReactNode }) {
-    return (
-        <Auth>{children}</Auth>
-    );
-}
--- src/helper/getSession.ts
+++
@@ -1,8 +0,0 @@
-"use server";
-import { authOptions } from "@root/app/api/auth/[...nextauth]/options";
-import { getServerSession } from "next-auth";
-
-const getSession = async () => {
-  return getServerSession(authOptions);
-};
-export default getSession;
